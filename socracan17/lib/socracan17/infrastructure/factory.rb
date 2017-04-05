fe62c9ea4bc358885fbaee3afa5149b22705dac5
Néstor Salceda <nestor.salceda@gmail.com>
2017-04-05 20:46:44 +0100

    Add method for create domain event subscribers

--- socracan17/lib/socracan17/infrastructure/factory.rb
+++ socracan17/lib/socracan17/infrastructure/factory.rb
@@ -12,6 +12,14 @@ module SocraCan17
       def json_serializer
         @json_serializer ||= JSONSerializer.new
       end
+
+      def domain_event_subscriber
+        @domain_event_subscriber ||= DomainEventSubscriber.new(amqp_client, json_serializer, logger)
+      end
+
+      def logger
+        @logger ||= Logger.new(STDOUT)
+      end
     end
   end
 end
