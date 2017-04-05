2772ebefb469ac0ce022ad218d8adfe112b30619
Néstor Salceda <nestor.salceda@gmail.com>
2017-04-05 13:17:59 +0100

    Add a /services endopoint and wire with SocraCan action dispatcher

--- http-api/app.rb
+++ http-api/app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
+require 'socracan17'
+
 
 post '/services' do
+  args = Hash[params["params"].map { |key, value| [key.to_sym, value] }]
 
+  SocraCan17::Actions::ACTION_DISPATCHER.dispatch(params["method"].to_sym, args)
 end
--- http-api/spec/services_spec.rb
+++ http-api/spec/services_spec.rb
@@ -1,6 +1,12 @@
 describe 'POST /services' do
-  it 'foobar' do
-    post '/services'
+  it 'uses SocraCan17 action dispatcher for dispatching commands on HTTP API' do
+    post '/services', method: :create_a_new_session, params: {
+      :title => 'Commands Surfing to Server',
+      :facilitator => 'Alberto Gualis & Néstor Salceda',
+      :datetime => 'Tomorrow',
+      :place => 'Beach',
+      :description => 'We will see how commands flow to server'
+    }
 
     expect(last_response).to be_ok
   end
