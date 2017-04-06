58069d1006dc4388eb4894820669257070d963e3
Néstor Salceda <nestor.salceda@gmail.com>
2017-04-06 10:41:18 +0100

    Wire up WebSockts with Domain-events notification

--- ws-api/app.rb
+++ ws-api/app.rb
@@ -1,7 +1,26 @@
 require 'websocket-eventmachine-server'
 
+require 'socracan17'
+
+require 'json'
+
+factory = SocraCan17::Infrastructure::Factory.new
+subscriber = factory.domain_event_subscriber
+
+class BroadcastDomainEventProcessor < SocraCan17::Infrastructure::DomainEventProcessor
+  def initialize(websocket)
+    @websocket = websocket
+  end
+
+  def process(event)
+    @websocket.send(JSON.dump(event))
+  end
+end
+
 EM.run do
   WebSocket::EventMachine::Server.start(:host => "0.0.0.0", :port => 9000) do |ws|
+    subscriber.subscribe('session.created', 'session.created', BroadcastDomainEventProcessor.new(ws))
+
     ws.onopen do
       puts "Client connected"
     end
