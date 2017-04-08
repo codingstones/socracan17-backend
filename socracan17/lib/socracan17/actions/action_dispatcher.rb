14a0990a4f54bafcf6ff11bdce98ad076b63d32d
Néstor Salceda <nestor.salceda@gmail.com>
2017-04-08 12:26:02 +0100

    Put it clear where actions are instanced

--- socracan17/lib/socracan17/actions/action_dispatcher.rb
+++ socracan17/lib/socracan17/actions/action_dispatcher.rb
@@ -12,8 +12,16 @@ module SocraCan17
     end
 
     def self.initialize
-      action_dispatcher.add_action(:create_a_new_session, CreateANewSession.new(@@repository_factory.session_repository, @@infrastructure_factory.domain_event_publisher))
-      action_dispatcher.add_action(:retrieve_all_sessions, RetrieveAllSessions.new(@@repository_factory.session_repository))
+      action_dispatcher.add_action(:create_a_new_session,
+        CreateANewSession.new(
+          @@repository_factory.session_repository,
+          @@infrastructure_factory.domain_event_publisher
+        )
+      )
+
+      action_dispatcher.add_action(:retrieve_all_sessions,
+        RetrieveAllSessions.new(@@repository_factory.session_repository)
+      )
     end
 
     initialize
